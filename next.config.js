fde3c4a1055e0b00425404fa6f11da92c271abd4
Aleksandra Shuianova <ashuyanova@seventest.ru>
2023-05-04 23:55:02 +0300

    feat: added backend url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://188.166.114.84/api/v1/:path*' // Proxy to Backend
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
