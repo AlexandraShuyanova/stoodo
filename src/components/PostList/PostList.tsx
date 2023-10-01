fbefd164f5266cade058c39a96e9bc94bbc51807
Aleksandra Shuianova <ashuyanova@seventest.ru>
2023-10-02 02:08:46 +0300

    fix:delete last fix

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -21,7 +21,7 @@ export const PostList = () => {
         }
 
         setPostList( list => [...list, ...data.content])
-    }, [data])
+    }, [data, postList])
 
     useEffect(() => {
         const handleScroll = () => {
